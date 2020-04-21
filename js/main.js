7aa7e7ce480c7cad1ef9a8b38a1996cae1cb3607
Giovanna Ramos <gikaaramos2013@gmail.com>
2020-04-21 17:32:43 -0300

    Aula 1 JavaScript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function botao(){
+    document.getElementById("a").innerHTML="Passe o mouse aqui";
+}
+
+function trocar(elemento){
+    elemento.innerHTML="Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    elemento.innerHTML="Passe o mouse aqui"
+}
